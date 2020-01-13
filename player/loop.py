6591ea3d8f5a9f23a834cb5bdca37a2ac9761e95
Patrick Delaney <pthomasdelaney@gmail.com>
2020-01-13 01:33:34 -0500

    refactor: __main__ and playloop modified to be testable

--- player/loop.py
+++ player/loop.py
@@ -1,11 +1,26 @@
 import requests
 import threading
+from signal import signal, SIGINT
 from time import sleep
 from streamer import stream  # type: ignore
 from ydl import ydl
 
 
-def playloop():
+def exit_signal(signal, frame):
+    """ Handler function used to exit the playloop. """
+    global interrupted
+    interrupted = True
+
+
+#: Registered exit_handler
+signal(SIGINT, exit_signal)
+
+
+#: Default loop state
+interrupted = False
+
+
+def playloop(interrupted=interrupted):
     """ A loop that runs forever.  This will attempt to grab a song from the
     tswf-api and download it.  Once it's downloaded the loop will call ffmpeg
     to play the song, waiting for it to finish before moving on.
@@ -14,17 +29,12 @@ def playloop():
     """
     while True:
         if stream.process is None or stream.process.poll() is not None:
-            next_song = requests.get("https://127.0.0.1/api/next", verify=False)
-            if "Next" in next_song.json():
-                try:
-                    ydl.download([next_song.json()["Next"]])
-                except Exception as e:
-                    print("Error downloading : ", next_song.json())
-                    raise e
-            else:
-                print("no songs")
-
-        sleep(5)
+            next_song = requests.get("http://127.0.0.1:8080/api/next", verify=False)
+            ydl.download(
+                [next_song.json()["Next"]]
+            ) if "Next" in next_song.json() else sleep(5)
+            if interrupted:
+                break  # check for interruption at the end for testing purposes.
 
 
 player = threading.Thread(name="player", target=playloop)
--- tests/test_player.py
+++
@@ -1,9 +0,0 @@
-import sys
-import pytest
-from pathlib import Path
-
-
-base_directory = Path(__file__).absolute().parent.parent
-sys.path.insert(0, str(base_directory / "player"))
-
-from player import Stream  # type: ignore
